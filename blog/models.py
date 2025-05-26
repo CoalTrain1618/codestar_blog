f0ae55644fd5b86be4bad6d51d782477095674eb
CaffiendCoder <175485343+CoalTrain1618@users.noreply.github.com>
2025-05-26 11:14:04 +0100

    Added new field to post and migrateed

--- blog/models.py
+++ blog/models.py
@@ -6,12 +6,12 @@ from django.contrib.auth.models import User
 STATUS = ((0, "Draft"), (1, "Published"))
 
 class Post(models.Model):
-    title = models.CharField(max_length=200, unique=True)
-    slug = models.SlugField(max_length=200, unique=True)
+    title = models.CharField(max_length=200, unique=True) # Only accepts python sting data with the use of .CharField
+    slug = models.SlugField(max_length=200, unique=True) # SlugField is used for URL-friendly representation of the title
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="blog_posts"
-    )
-    content = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
-    status = models.IntegerField(choices=STATUS, default=0)
-
+    ) # ForeignKey creates a many-to-one relationship with the User model.
+    content = models.TextField() # TextField is used for large text content box.
+    created_on = models.DateTimeField(auto_now_add=True) # DateTimeField automatically sets the date and time when the post is created.
+    status = models.IntegerField(choices=STATUS, default=0) # IntegerField is used to store the status of the post, with choices defined in STATUS tuple.
+    excerpt =  models.TextField(max_length=200, blank=True)
