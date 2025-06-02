be24fc2c42b8042bf9b82cbcd79fef1f006bff65
CaffiendCoder <175485343+CoalTrain1618@users.noreply.github.com>
2025-06-02 06:37:43 +0100

    Added content to posts. Added labeling to Classes for data readability

--- blog/models.py
+++ blog/models.py
@@ -16,6 +16,10 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0) # IntegerField is used to store the status of the post, with choices defined in STATUS tuple.
     excerpt =  models.TextField(max_length=200, blank=True)
     updated_on = models.DateTimeField(auto_now=True)
+    class Meta:
+        ordering = ["-created_on"]
+    def __str__(self):
+        return f"{self.title}| Written by {self.author}" 
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
@@ -23,3 +27,7 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        ordering = ["created_on"]
+    def __str__(self):
+        return f"{self.body} by {self.author}"
