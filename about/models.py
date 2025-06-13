f8414f87f49f8372121005f146d47850af48e669
CaffiendCoder <175485343+CoalTrain1618@users.noreply.github.com>
2025-06-13 07:41:40 +0100

    Fixed TypeError in models.py line 8

--- about/models.py
+++ about/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class About(models.Model):
     title = models.CharField(max_length=100,)
     content = models.TextField(max_length=300)
-    updated_on = models.DateTimeField(auto_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
