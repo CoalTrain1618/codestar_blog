3992974cfe2148b93d0e3e34821e9f03ae329b4d
CaffiendCoder <175485343+CoalTrain1618@users.noreply.github.com>
2025-06-16 07:44:08 +0100

    Created function based view in about views.py

--- about/views.py
+++ about/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import About
 
 # Create your views here.
+
+def about_detail(request):
+    queryset = About.objects.order_by("title", "content", "updated_on")
+    post = get_object_or_404(queryset)
+
+    return render(
+        request,
+        "about/about.html",
+        {
+            "post":post
+        }
+    )
