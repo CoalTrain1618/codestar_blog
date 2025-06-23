625354587c1c8fb78c078ba2cdb35656615599ae
CaffiendCoder <175485343+CoalTrain1618@users.noreply.github.com>
2025-06-23 10:19:04 +0100

    Add form submission functionatity for collaborating on the about page

--- about/views.py
+++ about/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.contrib import messages
 from .models import About
+from .forms import CollaborateForm
+
 
 # Create your views here.
 
@@ -9,13 +12,28 @@ def about_detail(request):
 
 
     """
+    if request.method == "POST":
+        collaborate_form = CollaborateForm(data=request.POST)
+        if collaborate_form.is_valid():
+            collaborate_form.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Collaboration request received! I endeavor to respond within 2 working days'
+            )
+
     about = About.objects.order_by("title", "content", "updated_on").first()
+    collaborate_form = CollaborateForm()
+
     if not about:
         return render(request, "about/about.html")
     return render(
         request,
         "about/about.html",
         {
-            "about":about
+            "about":about,
+            "collaborate_form":collaborate_form
         }
     )
+
+
+
